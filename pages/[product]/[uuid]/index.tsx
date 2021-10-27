47104db5a5df1d22554e86a284f7846764708f8c
Mário Santos <mario.santos@ifood.com.br>
2021-10-27 12:27:16 -0300

    pages config

---
+++ pages/[product]/[uuid]/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => (
+  <Head>
+    <title>Zuc</title>
+    <meta name="description" content="Zuc products" />
+  </Head>
+);
+
+export async function getServerSideProps(
+  { params }:
+  { params: { product: string; uuid: string } },
+) {
+  // cal API to get product
+  console.log(params);
+
+  return {
+    props: {},
+  };
+}
+
+export default Home;
